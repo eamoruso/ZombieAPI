648cfbb54227cf999c5cc2b7fd376a63cd56008a
Edward Amoruso <50423437+eamoruso@users.noreply.github.com>
2026-06-06 23:37:16 -0400

    Add files via upload

---
+++ Python_Web_API_server/run2.sh
@@ -0,0 +1,1 @@
+uvicorn app:app --host 0.0.0.0 --port 80 --ssl-keyfile key.pem --ssl-certfile cert.pem --reload
---
+++ Python_Web_API_server/start-small.sh
@@ -0,0 +1,4 @@
+python3 -m venv venv
+source venv/bin/activate
+pip install fastapi uvicorn jinja2
+uvicorn app-original:app --host 0.0.0.0 --port 80 --reload
